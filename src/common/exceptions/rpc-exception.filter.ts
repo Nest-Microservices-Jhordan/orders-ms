0a3668e508ebd50015ec2fef3374a180e0cf7481
Jhordan Alarcon <jhordan.rav@gmail.com>
2025-10-31 10:00:40 -0700

    Orders CRUD and prisma config

---
+++ src/common/exceptions/rpc-exception.filter.ts
@@ -0,0 +1,27 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { Observable, throwError } from 'rxjs';
+
+@Catch(RpcException)
+export class RpcCustomExceptionFilter implements ExceptionFilter<RpcException> {
+  catch(exception: RpcException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const rpcError = exception.getError();
+
+    if (
+      typeof rpcError === 'object' &&
+      'status' in rpcError &&
+      'message' in rpcError
+    ) {
+      const status = isNaN(Number(rpcError.status)) ? 400 : rpcError.status;
+      return response.status(status).json(rpcError);
+    }
+
+    response.status(401).json({
+      status: 400,
+      message: rpcError,
+    });
+  }
+}
